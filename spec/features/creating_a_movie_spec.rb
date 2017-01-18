152fe8907d9ef00a1d9d81bb1b56391edfadfc4d
Tebs <qatrera@gmail.com>
2017-01-18 22:09:18 +0100

    Add and update specs

--- spec/features/creating_a_movie_spec.rb
+++ spec/features/creating_a_movie_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
 feature "creating a movie" do
+  # by using let you can remove the instance variables.
+  # let! makes sure a before block is created so data is ready before the test starts
+  # let!(:genre) { create :genre}
+  # let!(:movie) { build :movie}
+
+  before do
+    @genre = create :genre
+    @movie = build :movie
+  end
+
   specify do
     visit '/'
     click_on 'New Movie'
 
     expect(page).to have_css 'h1', text: 'New Movie'
 
-    fill_in "Title", with: "Babe: Pig In The City"
-    fill_in "Year", with: "1998"
-    fill_in "Description", with: "It's really just an awesome movie."
+    fill_in "Title", with: @movie.title
+    fill_in "Year", with: @movie.year
+    fill_in "Description", with: @movie.description
+
+    # you can also do:
+    # select @genre.name, from: "Genre"
+    find(:select, "movie_genre_id").find(:option, @genre.name).select_option
+
     click_button "Create Movie"
 
-    expect(page).to have_css 'h1', text: 'Babe: Pig In The City (1998)'
+    expect(page).to have_css 'h1', text: "#{@movie.title} (#{@movie.year})"
   end
 end
