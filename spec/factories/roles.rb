3aac5135ecb2c76308523b42206a07e03943aaf8
Tebs <qatrera@gmail.com>
2017-01-18 22:11:09 +0100

    update roles factory

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :role do
     name { actor.male? ? FFaker::Name.male_name_with_prefix : FFaker::Name.female_name_with_prefix }
-    movie { Movie.order("RANDOM()").first }
-    actor { Actor.order("RANDOM()").first }
+    movie
+    actor
   end
 end
