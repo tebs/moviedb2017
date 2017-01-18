f365a0342fb0ef2b231433c490690343344d9517
Tebs <qatrera@gmail.com>
2017-01-18 22:15:24 +0100

    Add spec for unique movie:year validation

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe Movie, type: :model do
       expect(movie).to_not be_valid
     end
   end
+
+  context "with another movie of the same name and year" do
+  # another way to set the data
+  # let(:movie) { build :movie }
+  #
+  # let(:another_movie) do
+  #   create :movie, title: movie.title, year: movie.year
+  # end
+
+  before do
+    FactoryGirl.create(:movie, title: "Star Wars", year: 1977)
+  end
+
+  it 'is not valid' do
+
+    movie = build(:movie, title: "Star Wars", year: 1977)
+    expect(movie).to_not be_valid
+    # another way:
+    # expect(movie.valid?).to be_falsey
+
+    movie.year = 1978
+    expect(movie).to be_valid
+  end
+end
 end
